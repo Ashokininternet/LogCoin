d716bb7fdd62b2edc0ff3fd555e8330cd68cbd6e
Ashok <rajalekshmykomalam983@gmail.com>
2025-10-18 23:57:30 +0530

    Update-01
    Done the basic setup

--- app.js
+++ app.js
@@ -1,19 +1,28 @@
 const SHA256 = require('crypto-js/sha256');
 class CryptoBlock {
-    constructor(index, timestamp, data, precedingHash = "") {
+    constructor(index, timestamp, data, precedingHash = " ") {
         this.index = index;
         this.timestamp = timestamp;
         this.data = data;
         this.precedingHash = precedingHash;
         this.hash = this.computeHash();
+        this.nonce = 0;
     };
     computeHash() {
-        return SHA256(this.index + this.precedingHash + this.timestamp + JSON.stringify(this.data)).toString();
+        return SHA256(this.index + this.precedingHash + this.timestamp + JSON.stringify(this.data) + this.nonce).toString();
+    }
+    proofOfWork(difficulty){
+        while(this.hash.substring(0, difficulty) !==Array(difficulty + 1).join("0")){
+            this.nonce++;
+            this.hash = this.computeHash();
+        }
     }
 }
 class CryptoBlockchain {
     constructor() {
         this.blockchain = [this.startGenesisBlock()];
+        this.difficulty = 4;
+
     }
     startGenesisBlock() {
         return new CryptoBlock(0, "17/10/2025", "Initial Block in the Chain", "0");
@@ -23,8 +32,22 @@ class CryptoBlockchain {
     }
     addNewBlock(newBlock) {
         newBlock.precedingHash = this.obtainLatestBlock().hash;
-        newBlock.hash = newBlock.computeHash();
+        // newBlock.hash = newBlock.computeHash(); my old code for generating hash
+        newBlock.proofOfWork(this.difficulty);
         this.blockchain.push(newBlock);
+    }
+    checkChainValidity(){
+        for(let i = 1; i < this.blockchain.length; i++){
+            const currentBlock = this.blockchain[i];
+            const precedingBlock = this.blockchain[i -1];
+            if(currentBlock.hash !== currentBlock.computeHash()){
+                return false;
+            }
+            if(currentBlock.precedingHash !== precedingBlock.hash){
+                return false;
+            }
+            return true;
+        }
     }
 }
 let LogCoin = new CryptoBlockchain();
